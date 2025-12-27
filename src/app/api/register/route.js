0bed144be8c563471dd8c9ef4597343eb578270c
TalhaBajwa41 <bajwaitx41@gmail.com>
2025-12-27 12:58:56 +0500

    talha

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import bcrypt from 'bcryptjs';
-import User from '@/models/User';
+import User from '/@models/User';
 import { connectDB } from '@/lib/db';
 
 export async function POST(req) {
